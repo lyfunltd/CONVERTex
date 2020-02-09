1b2be89d5703cee969c5156176d567fb0681b9ea
lyfunltd <lyfunltd@gmail.com>
2020-02-09 10:35:38 +0200

    Completed flat list

--- app/index.js
+++ app/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import EStyleSheet from "react-native-extended-stylesheet";
 
-import Home from "./screens/Home";
+//import Home from "./screens/Home";
+import CurrencyList from "./screens/CurrencyList";
 
 EStyleSheet.build({
   $secondaryColor: "#3A3232",
   $white: "#FFFFFF",
   $lightGray: "#F0F0F0",
   $border: "#979797",
-  $inputText: "#797979"
+  $inputText: "#797979",
+  $darkText: "#343434",
+  $border: "#E2E2E2"
 });
 
 // eslint-disable-next-line react/display-name
-export default () => <Home />;
+//export default () => <Home />;
+export default () => <CurrencyList />;
